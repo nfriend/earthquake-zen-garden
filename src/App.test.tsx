cf715e47fbf10db5689bd6f24b4f7a2952071090
Nathan Friend <nathan@gitlab.com>
2021-04-10 08:25:12 -0400

    Add tests for App.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,10 +3,24 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
+const data = require('./data.json');
+
 describe('App.tsx', () => {
+  test('renders the app logo', () => {
+    render(<App />);
+    const logoElement = screen.getByAltText('Site logo');
+    expect(logoElement).toBeInTheDocument();
+  });
+
+  test('renders the title', () => {
+    render(<App />);
+    const titleElement = screen.getByText(data.site.title);
+    expect(titleElement).toBeInTheDocument();
+  });
+
   test('renders a link to the profile', () => {
     render(<App />);
-    const linkElement = screen.getByText(/Welcome Sally/i);
+    const linkElement = screen.getByText(`Welcome ${data.profile.firstName}`);
     expect(linkElement).toBeInTheDocument();
   });
 });
