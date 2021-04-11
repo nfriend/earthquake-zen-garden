8b0bdb972b923f6642ffb27b4b07978f576faee0
Nathan Friend <nathan@gitlab.com>
2021-04-10 20:10:57 -0400

    Make logo link to /earthquakes

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -6,7 +6,9 @@ const data = require('./data.json');
 function Nav(): JSX.Element {
   return (
     <nav className="flex items-center bg-nav p-4">
-      <img src={data.site.logoImage} alt="Site logo" className="h-10" />
+      <Link to="/earthquakes">
+        <img src={data.site.logoImage} alt="Site logo" className="h-10" />
+      </Link>
       <h1 className="flex-grow text-center text-2xl font-semibold text-title">
         {data.site.title}
       </h1>
