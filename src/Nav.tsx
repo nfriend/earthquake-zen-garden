f46f5b61a952c315dd77c19183ae1099ccfb8bd6
Nathan Friend <nathan@gitlab.com>
2021-04-10 21:55:51 -0400

    Fix responsive styles for logo

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -7,9 +7,13 @@ function Nav(): JSX.Element {
   return (
     <nav className="flex items-center bg-nav p-4">
       <Link to="/earthquakes">
-        <img src={data.site.logoImage} alt="Site logo" className="h-10" />
+        <img
+          src={data.site.logoImage}
+          alt="Site logo"
+          className="h-10 object-scale-down"
+        />
       </Link>
-      <h1 className="flex-grow text-center text-2xl font-semibold text-title">
+      <h1 className="flex-grow text-center text-lg sm:text-2xl font-semibold text-title mx-4">
         {data.site.title}
       </h1>
       <Link to="/profile">Welcome {data.profile.firstName}</Link>
